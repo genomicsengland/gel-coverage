3423f03f7d7894f6786da1eb08c866a41ff5974e
Matthew Parker <matthew.parker@genomicsengland.co.uk>
2016-03-01 15:32:32 +0000

    adding setup to install

---
+++ coverage-summary/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='gel-coverage',
+    version='',
+    packages=[''],
+    package_dir={'': 'coverage-summary'},
+    url='',
+    license='',
+    author='mparker',
+    author_email='matthew.parker@genomicsengland.co.uk',
+    description=''
+)
