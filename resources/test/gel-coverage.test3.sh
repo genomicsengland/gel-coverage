cb2aa6f140fe9bf2b5c3c3bb660a948dbe6b9929
uid=pferreiro <pferreiro@bio-pp9-01.biohpc.gel.zone>
2016-12-05 17:55:51 +0000

    creating wrappers for tests 3 and 4

---
+++ resources/test/gel-coverage.test3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$ -N gel-coverage.test3
+#$ -q all.q
+#$ -S /bin/bash
+#$ -o /home/pferreiro/src/gel-coverage/resources/test/gel-coverage.test3.log
+#$ -j y
+time /genomes/software/apps/python2.7-coverage_tests/bin/python /home/pferreiro/src/gel-coverage/scripts/exon_coverage_summary.py --bw /genomes/analysis/by_date/2016-09-27/HX01166477/CancerLP3000079-DNA_F03_NormalLP3000067-DNA_C12/coverage/LP3000079-DNA_F03.bw --config /home/pferreiro/src/gel-coverage/resources/exon_coverage_summary.config  --output /home/pferreiro/src/gel-coverage/resources/test/gel-coverage.test3.json
