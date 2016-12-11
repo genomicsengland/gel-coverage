a09a365153276aa66fb78a47f151cbdfe2eba6df
uid=pferreiro <pferreiro@bio-pp9-01.biohpc.gel.zone>
2016-12-11 20:37:01 +0000

    scripts for tests 9 and 10

---
+++ resources/test/gel-coverage.test9.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$ -N gel-coverage.test9
+#$ -q all.q
+#$ -S /bin/bash
+#$ -o /home/pferreiro/src/gel-coverage/resources/test/gel-coverage.test9.log
+#$ -j y
+time /genomes/software/apps/python2.7-coverage_tests/bin/python /home/pferreiro/src/gel-coverage/scripts/bigwig_analyser.py --bw /genomes/analysis/by_date/2016-09-27/HX01166477/CancerLP3000079-DNA_F03_NormalLP3000067-DNA_C12/coverage/LP3000079-DNA_F03.bw --config /home/pferreiro/src/gel-coverage/resources/exon_coverage_summary.config  --output /home/pferreiro/src/gel-coverage/resources/test/gel-coverage.test9.json --wg-regions /home/pferreiro/src/gel-coverage/resources/Homo_sapiens.GRCh37.75.dna.primary_assembly.NonN_Regions.CHR.bed --panel "Familial colon cancer" --panel-version 1.3
