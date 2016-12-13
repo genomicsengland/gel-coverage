800a64a5a3d801869aeaa9a16e0dbe45aa115b40
priesgo <pablo.riesgo-ferreiro@genomicsengland.co.uk>
2016-12-13 15:56:03 +0000

    #20 fixing script to local installation

--- scripts/bam2bigwig.sh
+++ scripts/bam2bigwig.sh
@@ -3,7 +3,8 @@ bigwig=$2
 
 
 java \
-  -jar /genomes/software/apps/gel-coverage/bam2wig/gel-coverage-jar-with-dependencies.jar \
-  -bam ${bam} \
-  -output - | \
-    /genomes/software/src/ucsc/wigToBigWig stdin $bam.chr $bigwig
+  -jar /home/pferreiro/src/gel-coverage//bam2wig/gel-coverage-jar-with-dependencies.jar \
+  --bam ${bam} \
+  --output -
+  --config /home/pferreiro/src/gel-coverage/resources/bigwig_analyser.config | \
+/genomes/software/src/ucsc/wigToBigWig stdin $bam.chr $bigwig
