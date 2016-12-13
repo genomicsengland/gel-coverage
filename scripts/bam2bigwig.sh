51810cc6b4cd59df73b208c707bbdc18b3093951
priesgo <pablo.riesgo-ferreiro@genomicsengland.co.uk>
2016-12-13 16:21:02 +0000

    and fixing script again...

--- scripts/bam2bigwig.sh
+++ scripts/bam2bigwig.sh
@@ -5,6 +5,6 @@ bigwig=$2
 /genomes/software/apps/jdk1.8.0_45/bin/java \
   -jar /home/pferreiro/src/gel-coverage//bam2wig/gel-coverage-jar-with-dependencies.jar \
   --bam ${bam} \
-  --output - \
+  --wig - \
   --config /home/pferreiro/src/gel-coverage/resources/bigwig_analyser.config | \
 /genomes/software/src/ucsc/wigToBigWig stdin $bam.chr $bigwig
