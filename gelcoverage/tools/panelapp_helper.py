099ff1a0c54d292863a6f2d4a589797c7dd88e06
MattWellie <matthew.welland@genomicsengland.co.uk>
2021-05-05 10:57:38 +0100

    BCB-219 force int confidence check

--- gelcoverage/tools/panelapp_helper.py
+++ gelcoverage/tools/panelapp_helper.py
@@ -1,6 +1,7 @@
 import logging
 from pypanelapp.python_panel_app_client import PanelApp
 
+
 class PanelappHelper:
 
     def __init__(self, host, assembly):
@@ -12,17 +13,22 @@ class PanelappHelper:
         """
         Gets the HGNC gene names in a given panel identified by panel name and panel version.
         Also, if provided, only gets those genes having a level of evidence listed in gene_confidence_threshold.
-        :param panel: the panel name
-        :param panel_version: the panel version
-        :param gene_confidence_threshold: the gene's level of evidence
+
+        :param str panel: the panel name
+        :param str panel_version: the panel version
+        :param int gene_confidence_threshold: the gene's level of evidence
         :return: a list of HGNC gene names
+        :rtype: list
         """
+
         logging.debug("Getting gene list from PanelApp...")
         try:
             panel = self.client.panel_get(panel_id=panel, version=panel_version)
         except:
             raise SystemError("PanelApp returned an error for the panel {}, version {}".format(panel, panel_version))
+
+        # force an integer comparison here
         gene_list = [gene.gene_data.gene_symbol for gene in panel.genes
-                     if int(gene.confidence_level) >= gene_confidence_threshold]
+                     if int(gene.confidence_level) >= int(gene_confidence_threshold)]
         logging.debug("Gene list obtained from PanelApp of %s!" % str(len(gene_list)))
         return gene_list
